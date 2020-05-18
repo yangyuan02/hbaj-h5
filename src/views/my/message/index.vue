863478dcdcf328f8c2a74f082f87f1785f16d850
yangyuan <1367511704@qq.com>
2020-05-18 22:13:05 +0800

    fix(消息页面调整)

--- src/views/my/message/index.vue
+++ src/views/my/message/index.vue
@@ -2,12 +2,12 @@
  * @Author: yangyuan
  * @Date: 2020-04-19 18:58:48
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-04-19 19:16:49
+ * @LastEditTime: 2020-05-18 22:12:37
  * @Description: 
  -->
 <template>
   <div class="page-view">
-    <div class="scroll-view-wrapper">
+    <div class="scroll-view-wrapper" :class="{'menu-pBottom' :menupB}">
       <Header title="我的通知"></Header>
       <div class="message-content">
         <List></List>
@@ -24,7 +24,9 @@ import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
 export default {
     data() {
-        return {};
+        return {
+            menupB: true
+        };
     },
     components: {
         List,
@@ -35,11 +37,7 @@ export default {
 </script>
 <style lang="less">
 .message-content {
-    background: linear-gradient(
-        180deg,
-        rgba(15, 79, 168, 1) 0%,
-        rgba(15, 79, 168, 0.98) 100%
-    );
+    background: linear-gradient(180deg, rgba(15, 79, 168, 1) 0%, rgba(15, 79, 168, 0.98) 100%);
     padding: 0.18rem 0;
 }
 </style>
