4f0f7503c8289a0c88c4ba20a9b5e36433d00da9
yangyuan <1367511704@qq.com>
2020-05-24 22:22:24 +0800

    fix(我的信息页面调整)

--- src/views/my/person/index.vue
+++ src/views/my/person/index.vue
@@ -2,17 +2,77 @@
  * @Author: yangyuan
  * @Date: 2020-04-17 21:38:27
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-05-19 23:40:31
+ * @LastEditTime: 2020-05-24 22:20:49
  * @Description: 
  -->
 <template>
   <div class="page-view">
-    <div class="scroll-view-wrapper">
+    <div class="scroll-view-wrapper" :class="{ 'menu-pBottom': menupB }">
       <div class="my-content">
         <my-header :info="user"></my-header>
         <div class="person-box">
-          <div class="role"></div>
-          <div class="organization"></div>
+          <div class="role common">
+            <div class="thumb"></div>
+            <div class="name">
+              <span>港口检察官</span>
+            </div>
+            <div class="tag">
+              <span>角色</span>
+            </div>
+          </div>
+          <div class="organization common">
+            <div class="thumb"></div>
+            <div class="name">
+              <span>宝山海事局</span>
+            </div>
+            <div class="tag">
+              <span>机构</span>
+            </div>
+          </div>
+          <div class="auth-box">
+            <div class="title">
+              <span>豪华游轮访问权限</span>
+            </div>
+            <div class="major-box">
+              <div class="left-major">
+                <ul>
+                  <li class="active">技术解读</li>
+                  <li>专业英语</li>
+                  <li>模拟训练</li>
+                </ul>
+              </div>
+              <div class="right-major">
+                <div class="item" v-for="(item, index) in 16">
+                  <span>救生</span>
+                </div>
+              </div>
+            </div>
+            <div class="tag">
+              <span>权限</span>
+            </div>
+          </div>
+          <div class="auth-box">
+            <div class="title">
+              <span>豪华游轮访问权限</span>
+            </div>
+            <div class="major-box">
+              <div class="left-major">
+                <ul>
+                  <li class="active">技术解读</li>
+                  <li>专业英语</li>
+                  <li>模拟训练</li>
+                </ul>
+              </div>
+              <div class="right-major">
+                <div class="item" v-for="(item, index) in 16">
+                  <span>救生</span>
+                </div>
+              </div>
+            </div>
+            <div class="tag">
+              <span>权限</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -29,7 +89,8 @@ import store from "@/widget/store";
 export default {
     data() {
         return {
-            user: {}
+            user: {},
+            menupB: true
         };
     },
     components: {
@@ -58,56 +119,134 @@ export default {
 </script>
 
 <style lang="less">
-.my-content {
-    background: rgba(245, 245, 245, 1);
-    height: 100%;
-    .my-list-content {
-        width: 7rem;
-        height: calc(100% - 3.9rem);
-        background: #fff;
-        margin: 0 auto;
-        border-radius: 0.21rem 0.21rem 0 0;
-        transform: translateY(-0.5rem);
-        ul {
-            padding-left: 0.38rem;
-            padding-right: 0.4rem;
-            li {
-                padding-top: 0.26rem;
-                padding-bottom: 0.26rem;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                border-bottom: 0.02rem solid rgba(238, 238, 238, 1);
-                .info {
-                    i {
-                        font-size: 0.32rem;
-                    }
-                    span {
-                        font-size: 0.28rem;
-                        font-family: MicrosoftYaHei;
-                        color: rgba(51, 51, 51, 1);
-                        line-height: 0.37rem;
-                        -webkit-background-clip: text;
-                        margin-left: 0.3rem;
-                    }
-                    .unreadCount {
-                        width: 0.44rem;
-                        height: 0.44rem;
-                        background: rgba(238, 27, 27, 1);
-                        text-align: center;
-                        line-height: 0.44rem;
-                        border-radius: 50%;
+.person-box {
+    padding: 0.27rem 0.35rem;
+    background: #fff;
+    min-height: 100%;
+    .common {
+        display: flex;
+        align-items: center;
+        background: rgba(15, 79, 168, 1);
+        padding: 0.16rem;
+        position: relative;
+        .thumb {
+            width: 1.2rem;
+            height: 1.2rem;
+            background: #f0f0f0;
+            margin-left: 0.5rem;
+            margin-right: 0.3rem;
+        }
+        .name {
+            font-size: 0.28rem;
+            font-family: MicrosoftYaHei;
+            color: rgba(255, 255, 255, 1);
+            line-height: 0.37rem;
+            letter-spacing: 0.01rem;
+            -webkit-background-clip: text;
+        }
+        .tag {
+            width: 1.3rem;
+            height: 0.4rem;
+            background: rgba(224, 32, 32, 1);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border-radius: 0.2rem 0px 0px 0.2rem;
+            position: absolute;
+            right: 0;
+            top: 0.16rem;
+            span {
+                color: #fff;
+            }
+        }
+    }
+    .organization {
+        margin: 0.16rem 0;
+        .tag {
+            background: rgba(255, 213, 26, 1);
+        }
+    }
+    .auth-box {
+        position: relative;
+        width: 6.8rem;
+        height: 3.51rem;
+        background: rgba(15, 79, 168, 1);
+        border-radius: 0.1rem;
+        padding-left: 0.2rem;
+        padding-top: 0.2rem;
+        margin-bottom: 0.16rem;
+        &:last-child {
+            margin-bottom: 0rem;
+        }
+        .title {
+            span {
+                color: #fff;
+            }
+        }
+        .major-box {
+            display: flex;
+            margin-top: 0.2rem;
+            .left-major {
+                ul {
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-around;
+                    height: 100%;
+                    li {
                         color: #fff;
-                        display: inline-block;
+                        position: relative;
+                        padding-left: 0.1rem;
+                        &.active {
+                            &::before {
+                                content: "";
+                                height: 100%;
+                                width: 0.04rem;
+                                position: absolute;
+                                left: 0;
+                                top: 0;
+                                background: rgba(255, 157, 59, 1);
+                            }
+                        }
                     }
                 }
-                .arrows {
-                    i {
-                        transform: rotate(180deg);
-                        font-size: 0.24rem;
-                        color: rgba(153, 153, 153, 1);
+            }
+            .right-major {
+                width: 4.63rem;
+                height: 2.6rem;
+                background: rgba(0, 145, 255, 1);
+                border-radius: 0.1rem;
+                margin-left: 0.16rem;
+                padding: 0.1rem;
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
+                .item {
+                    width: 0.9rem;
+                    height: 0.4rem;
+                    border-radius: 0.04rem;
+                    border: 0.01rem solid rgba(255, 255, 255, 1);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    span {
+                        color: #fff;
                     }
                 }
+            }
+        }
+        .tag {
+            position: absolute;
+            width: 1.3rem;
+            height: 0.4rem;
+            background: rgba(255, 157, 59, 1);
+            border-radius: 0.2rem 0px 0px 0.2rem;
+            right: 0;
+            top: 0.2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            span {
+                color: #fff;
             }
         }
     }
