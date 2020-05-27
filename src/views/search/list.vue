974e51c13a4e57b28114cdf85a548e220df5c92e
yangyuan <1367511704@qq.com>
2020-05-28 00:05:30 +0800

    fix(搜索结果页调整)

--- src/views/search/list.vue
+++ src/views/search/list.vue
@@ -2,7 +2,7 @@
  * @Author: yangyuan
  * @Date: 2020-04-15 23:40:15
  * @Email: 1367511704@qq.com
- * @LastEditTime: 2020-05-27 23:40:49
+ * @LastEditTime: 2020-05-28 00:04:45
  * @Description: 
  -->
 <template>
@@ -11,7 +11,17 @@
       <CourseItem v-for="(item, index) in [1, 2, 3, 4, 5, 6]" :key="index"></CourseItem>
     </div>
     <div class="no-list" v-else>
-      暂无课件
+      <div class="tips-box">
+        <div class="no-icon">
+          <i class="iconfont iconkongxiangzi"></i>
+        </div>
+        <div class="tips">
+          <span>
+            暂无内容
+          </span>
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -40,6 +50,24 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+        .tips-box {
+            .no-icon {
+                text-align: center;
+                i {
+                    color: rgba(40, 104, 240, 1);
+                    font-size: 1rem;
+                }
+            }
+            .tips {
+                span {
+                    font-size: 0.24rem;
+                    font-family: MicrosoftYaHei;
+                    color: rgba(102, 102, 102, 1);
+                    line-height: 0.31rem;
+                    -webkit-background-clip: text;
+                }
+            }
+        }
     }
 }
 </style>
