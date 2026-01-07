6b97b659e8456ef89dc71831f3a678ccaf536a90
yangyuan <1367511704@qq.com>
2026-01-07 15:16:35 +0800

    feat: 登录鉴权

--- src/layouts/DefaultLayout/index.vue
+++ src/layouts/DefaultLayout/index.vue
@@ -19,6 +19,21 @@
   </view>
 </template>
 
+<script setup>
+  import { onLoad } from '@dcloudio/uni-app'
+  import useAuthStore from '@/store/auth'
+  import { useRouter } from '@/router'
+  const router = useRouter()
+
+  const authStore = useAuthStore()
+  const { isLogined } = authStore;
+  onLoad(() => {
+    if (!isLogined) {
+      router.push({name: 'login'})
+    }
+  })
+</script>
+
 <style scoped lang="less">
 .layout {
   display: flex;
--- src/layouts/FrameInLayout/index.vue
+++ src/layouts/FrameInLayout/index.vue
@@ -1,9 +1,26 @@
 <!-- layouts/FrameInLayout.vue -->
 <template>
-  <view className="layout-container">
+  <view class="layout-container">
+    <slot />
   </view>
 </template>
-
 <script setup>
-// 组件逻辑
+  import { onLoad } from '@dcloudio/uni-app'
+  import useAuthStore from '@/store/auth'
+  import { useRouter } from '@/router'
+  const router = useRouter()
+
+  const authStore = useAuthStore()
+  const { isLogined } = authStore;
+  onLoad(() => {
+    if (!isLogined) {
+      router.push({name: 'login'})
+    }
+  })
 </script>
+
+<style scoped lang="less">
+.layout-container {
+  height: 100vh;
+}
+</style>
